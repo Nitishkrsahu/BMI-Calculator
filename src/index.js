953a7c257c1d410dc1cd6882030b47c71fe29b06
Nitish <nitishkrsahu123@gmail.com>
2025-04-22 15:52:45 +0530

    some more functionlity is added in BMI

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ function bmiCal(){
     let userWeight = parseInt(document.getElementById("weight").value);
     let userAge = parseInt(document.getElementById("age").value);
     if(isNaN(userHeight) | isNaN(userWeight)| isNaN(userAge)){
-        document.getElementById("bmiData").innerHTML = "Please provide Data";
+        let dataPrint = document.getElementById("bmiData");
+        dataPrint.innerHTML = "Please provide All Data!".fontcolor('red');
     }
     else{
         let BMI = userWeight/Math.pow(userHeight,2);
-        document.getElementById("bmiData").innerHTML = `your Age is  = ${userAge}<br> calculated BMI = ${BMI}`;
+        let roundedBmi = BMI.toFixed(2);
+        let dataPrintText = document.getElementById("bmiText");
+        dataPrintText.innerHTML = "Success!!".fontcolor('green');
+        let dataPrint = document.getElementById("bmiData");
+        dataPrint.innerHTML  = `your Age = ${userAge}<br> calculated BMI = ${roundedBmi}`;
     }
+}
+function bmiReset(){
+    document.getElementById("height").value = "";
+    document.getElementById("weight").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("bmiData").innerHTML = "";
+    document.getElementById("bmiText").innerHTML = "";
+    document.getElementById("bmiData").innerHTML = "";
 }
